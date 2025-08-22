7e224a476260251f673469db60de765dd6fffaf2
vafelkis <karolisstru@gmail.com>
2025-08-22 18:00:18 +0300

    troetero

--- katcanintospace.js
+++ katcanintospace.js
@@ -86,6 +86,7 @@ window.onload = function () { // Example event date
     //  elementx.innerText = displayTimeSinceEvent(elementx.innerText);
     //}
     //}
+    console.log("sigma");
     document.getElementById("splash").innerText = stext;
 const katlogo = ["katlogo.png","katlogoalt1.png","katlogoalt2.png"];
     document.getElementById("katrewkatelogo").getAttribute("src") = "../" + katlogo[Math.floor(Math.random() * katlogo.length)];
