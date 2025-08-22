d9f2bf84b3f1edd8bf8d8235601f8c929147994b
vafelkis <karolisstru@gmail.com>
2025-08-22 18:08:08 +0300

    troeeto

--- short.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () { 
-console.log("sigma");
-    document.getElementById("splash").innerText = stext;
-const katlogo = ["katlogo.png","katlogoalt1.png","katlogoalt2.png"];
-    document.getElementById("katrewkatelogo").getAttribute("src") = "../" + katlogo[Math.floor(Math.random() * katlogo.length)];
-}
