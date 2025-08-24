760c80df1bc89dc9577c9b4cd59d67d4d87f611d
vafelkis <karolisstru@gmail.com>
2025-08-24 12:00:10 +0300

    troetero

--- katcanintospace.js
+++ katcanintospace.js
@@ -78,8 +78,9 @@ const stext = bish[Math.floor(Math.random() * bish.length)];
 // Auto-run on page load
 window.onload = function () { // Example event date
     var path = window.location.pathname;
-    document.body.innerHTML = "<style>#topmessage {  position: fixed !important;  top: 0 !important;  left: 0 !important;  width: 100% !important;  background: orange !important;  color: white !important;  text-align: center !important;  padding: 0px !important;  z-index: 999999 !important;  font-size: 20px !important;}</style><div class='scrolldiv' id='topmessage'><div class='scrolltext'>katdustries is currently offering a sponsorship program, anyone who wants to contribute to the website and become a sponsor [misc page], contact katrewkate for more information! there are no requirements to enter, besides being chill. 😊 <i>spots are limited!</i></div></div>"+ document.body.innerHTML;
+    document.body.innerHTML = "<style>#topmessage {  position: fixed !important;  top: 0 !important;  left: 0 !important;  width: 100% !important;  background: orange !important;  color: white !important;  text-align: center !important;  padding: 0px !important;  z-index: 999999 !important;  font-size: 20px !important;}</style><div class='scrolldiv' id='topmessage'><div class='scrolltext'>katdustries is currently offering a sponsorship program, anyone who wants to contribute to the website and become a sponsor [misc page], contact katrewkate for more information! there are no requirements to enter, besides being chill. 😊 spots are limited!</div></div>"+ document.body.innerHTML;
     setInterval(startclock, 1000); 
+    console.log("started")
     //if (page === "index.html") {
     displayTimeSinceEvent(new Date("2024-09-19"), 'boatcreationdateago');
     displayTimeSinceEvent(new Date("2022-05-30"), 'bccreationdateago');
@@ -179,6 +180,8 @@ function startclock() {
     const [day, month, year] = date.split('/');
     date1.innerText = `${year}-${month}-${day}`
     date2.innerText = time;
+    console.log(`${year}-${month}-${day}` + " " + time)
+
 }
 const audio = document.getElementById("msc");
 const txtaud = document.getElementById("stopmsc");
