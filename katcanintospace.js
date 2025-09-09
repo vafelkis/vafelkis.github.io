485750dcbb173e2f3cf457076b3459d4befbe9e3
vafelkis <karolisstru@gmail.com>
2025-09-09 21:33:51 +0300

    treorteo

--- katcanintospace.js
+++ katcanintospace.js
@@ -77,9 +77,7 @@ function displayTimeSinceEvent(eventDate, elementId = null, endEventDate = null,
 
 const bish = ["whoever reads is 'coustic", "check out Pancair Development Faciltes!", "dont forget to checkout vucic", "have you went to misc?", "good things start with b, boat, blyat.. bodka, bilnius!", "Bilnius", "eurobeat in the 26", "aproved by gogel", "bbbbbb"]
 const stext = bish[Math.floor(Math.random() * bish.length)];
-document.getElementById("loadshit").onload = function () {
-    document.body.insertAdjacentHTML("afterbegin", "<style>#topmessage {  position: fixed !important;  top: 0 !important;  left: 0 !important;  width: 100% !important;  background: lightblue !important;  color: white !important;  text-align: center !important;  padding: 0px !important;  z-index: 999999 !important;  font-size: 20px !important;}</style><div class='scrolldiv' id='topmessage'><div class='scrolltext'>DFIKFKFKEIOJSTY IRTY JIEJIYRT JIDTYHKMCGJIDCYUJRTIEY JE, если вы этого читаете, тогда вы незнаю, какие-то лунатики вообще хахахаха:) удачи вам всем!</div></div>");
-}
+
 // Auto-run on page load
 window.onload = function () { // Example event date
     console.log("started")
@@ -199,6 +197,9 @@ function startclock() {
 }
 const audio = document.getElementById("msc");
 const txtaud = document.getElementById("stopmsc");
+document.getElementById("loadshit").onload = function () {
+    document.body.insertAdjacentHTML("afterbegin", "<style>#topmessage {  position: fixed !important;  top: 0 !important;  left: 0 !important;  width: 100% !important;  background: lightblue !important;  color: white !important;  text-align: center !important;  padding: 0px !important;  z-index: 999999 !important;  font-size: 20px !important;}</style><div class='scrolldiv' id='topmessage'><div class='scrolltext'>DFIKFKFKEIOJSTY IRTY JIEJIYRT JIDTYHKMCGJIDCYUJRTIEY JE, если вы этого читаете, тогда вы незнаю, какие-то лунатики вообще хахахаха:) удачи вам всем!</div></div>");
+}
 function playmsc() {
     setTimeout(function () {
         // Code you want to run after 5 seconds
