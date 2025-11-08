614c442a2674c36ac2896d9931221242acaa2c33
vafelkis <karolisstru@gmail.com>
2025-11-08 16:57:01 +0200

    troetreo

--- sigma.js
+++ sigma.js
@@ -1,5 +1,5 @@
-const quotes = [ "hi", "goodbye", "sigma"];
-const srcs = [ "/katvodka logo alt.png", "/eurobeatcanadawhite.png", "/katlogowhite.png"];
+const quotes = [ "hi", "goodbye", "sigma", "have you seen the misc page, m8?", "if you are reading this, you are very sigma.", "\"WHAT THE FUCK DID YOU DO?\"", "\"it is only after we've lost everything that we are free to do anything.\"", "\"NIIIIIIIIIIII\""];
+const srcs = [ "/Katvodka logo alt.png", "/eurobeatcanadawhite.png", "/katlogowhite.png"];
 const quote = quotes[Math.floor(Math.random() * srcs.length)];
 const src = srcs[Math.floor(Math.random() * srcs.length)];
 document.getElementById("quote").innerText = quote;
