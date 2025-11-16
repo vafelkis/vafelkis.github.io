44d0c7a7aab23dc9a305292210b73ff574c31bae
vafelkis <karolisstru@gmail.com>
2025-11-16 18:59:50 +0200

    treotreo

--- kat/archive/300thcommit/katcanintospace.js
+++ kat/archive/300thcommit/katcanintospace.js
@@ -1,16 +1,3 @@
-let date1 = document.getElementById("date1");
-let date2 = document.getElementById("date2");
-
-document.body.insertAdjacentHTML("afterbegin", `
-    <div id="loading" style="position:fixed; scroll:none; display:flex; height: 100%; width: 100%; left: 0; top:0; background: linear-gradient(45deg, #010b29c2 0%, #010b2993 100%); z-index: 9999; text-align: center;">
-    <div style="margin: auto auto; font-size:150%;" ><img src="/Katvodkalogoalt.png" style="width: 20%; object-fit: contain; filter: drop-shadow(3px 3px 0 black);" id="logo"><br>loading...<br>
-    <a style="all:unset; font-size: 80%;">do not panic if it looks stuck / site looks broken.<br>the website will look incomplete, while loading.<div style="opacity:0.5; font-size:16px; width: 75%; margin: 0 auto;">in the case of it freezing, and you see that nothing is loading, please refresh the page. if it still does not work, well... then youre cooked.😭 nah but fr, pls contact katrewkate@gmail.com, or if you can access the poptart page (https:/vafelkis.github.io/poptart) then contact from there please! this pleasant [or horibbel.. :(] experience, has been brought to you by katrewkate/katdustries incorioaprtped induastries in some offshore country that no one knows about ! 
-    </a><div id="quote">some fancy ahh quote here 🤓</div></div>`);
-
-reset();
-//document.body.insertAdjacentHTML("afterbegin", "<style>#topmessage {  position: fixed !important;  top: 0 !important;  left: 0 !important;  width: 100% !important;  background: lightblue !important;  color: white !important;  text-align: center !important;  padding: 0px !important;  z-index: 999999 !important;  font-size: 20px !important;}</style><div class='scrolldiv' id='topmessage'><div class='scrolltext' style='font-size: 25px;' id='topbannerad' >katrewdustries is celebrating boat's first anniversary! pls see boat / news pages for more information. 🎂🍰🍰🎂🍥🎉🎉🎉 BIRTHDAY UPDATE CAME OUT ALREADY, GO CHECK IT OUT. REWARDS LIMITED TO A WEEK! 🎂🍰🍰🎂🍥🎉🎉🎉</div></div>");
-
-const fmt = Intl.DateTimeFormat({ timeZone: 'Europe/Vilnius' });
 function calculateTimeSinceEvent(eventDate, endDate = null) {
     let now = new Date();
     let event = new Date(eventDate);
@@ -41,16 +28,7 @@ function calculateTimeSinceEvent(eventDate, endDate = null) {
     return { years, months, days };
 }
 
-function sigma(href) {
-    const va = document.getElementById("swoosh")
 
-    va.currentTime = 0;
-    va.play();
-    va.onended = function () {
-        window.location.assign(window.location.origin + href);
-    };
-
-}
 // Function to display time since event
 function displayTimeSinceEvent(eventDate, elementId = null, endEventDate = null, language = null) {
     const time = calculateTimeSinceEvent(eventDate, endEventDate);
@@ -69,12 +47,12 @@ function displayTimeSinceEvent(eventDate, elementId = null, endEventDate = null,
     }
     else if (language == "lt") {
 
-        result = `prieš ${years} metų, ${months} mėnesių ir ${days} dienų`;
+        result = `${years} metų, ${months} mėnesių ir ${days} dienų`;
         console.log("c");
     }
     else if (language == "ru") {
 
-        result = `${years} года, ${months} месяца и ${days} дней назад`;
+        result = `${years} лет, ${months} месяцев и ${days} дней`;
         console.log("d");
     }
 
@@ -85,52 +63,25 @@ function displayTimeSinceEvent(eventDate, elementId = null, endEventDate = null,
     console.log("e");
 }
 
-const bish = ["whoever reads is 'coustic", "check out Pancair Development Faciltes!", "dont forget to checkout vucic", "have you went to misc?", "good things start with b, boat, blyat.. bodka, bilnius!", "Bilnius", "eurobeat in the 26", "aproved by gogel", "bbbbbb", "welcome to the real world.", "do not support andrwe tate", "ba bla bla bla", "YAPPPPPPPPPP", "welcome from 1984", "if you read, you gey.", "check out new shi bro.", "live, laugh, love vlc", "sigma reader", "wtf bro", "HAHAHAHA"]
+const bish = ["whoever reads is 'coustic", "check out Pancair Development Faciltes!", "dont forget to checkout vucic", "have you went to misc?","good things start with b, boat, blyat.. bodka, bilnius!", "Bilnius", "eurobeat in the 26", "aproved by gogel", "bbbbbb"]
 const stext = bish[Math.floor(Math.random() * bish.length)];
 
-window.onload = function () { 
-    
-    for (element of document.getElementsByClassName("dateagodays")) {
-        console.log(element.id);
-        el = document.getElementById(`${element.id}-agoel`)
-        const time = calculateTimeSinceEvent(element.innerText, new Date());
-
-        if (!time) return;
-        const { years, months, days } = time;
-        let result = "";
-        console.log("a");
-        result = `(${years} yrs, ${months} mnths, ${days} d ago)`;
-        console.log("b");
-
-        el.innerText = result;
-        console.log("e");
-    }
-    const box = document.getElementById('loading');
-    box.classList.add('sigmaxxx');
-    console.log("started")
+// Auto-run on page load
+window.onload = function () { // Example event date
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
     //if (page === "index.html") {
     displayTimeSinceEvent(new Date("2024-09-19"), 'boatcreationdateago');
     displayTimeSinceEvent(new Date("2022-05-30"), 'bccreationdateago');
     displayTimeSinceEvent(new Date("2025-01-15"), 'websitecreationago');
-    displayTimeSinceEvent(new Date("2007-09-26"), 'katdob');
-
-
-
-    setInterval(startclock, 500);
+    
     let maythernggods = new Date();
     let noetter = maythernggods.getHours();
     console.log(noetter);
     if (noetter > 19 || noetter < 5) {
         console.log("oui baguette bonjour")
         document.getElementById("altbodyhtml").style.backgroundImage = "url('nightwatchtower.gif')";
-        document.getElementById("katrewkatelogo").setAttribute("src", "katlogo.png");
         document.getElementById("altbodyhtml").style.setProperty('background-image', 'url("nightwatchtower.gif")', 'important');
-        document.querySelectorAll('.orangez').forEach((elem) => {
-            console.log(elem.innerText);
-            elem.classList.remove("orangez");
-            elem.classList.add("lightblue");
-
-        });
     }
     //for (elementx of document.getElementsByClassName("datex")) {
     //  console.log(elementx);
@@ -139,62 +90,16 @@ window.onload = function () {
     //}
     console.log("sigma");
     document.getElementById("splash").innerText = stext;
-    date1 = document.getElementById("date1");
-    date2 = document.getElementById("date2");
-
+    
+    
 
 }
-if (!window.location.href.includes("vafelkis.github.io/newz")) {
-document.body.insertAdjacentHTML("afterbegin", '<div id="scrollthing" style="position: fixed; top: 0;right: 0;width: 1vw;height: 100vh;background: linear-gradient(180deg, rgba(255, 255, 255, 0.19) 80%, rgba(255, 255, 255, 0.56) 100%); margin: 0;padding: 0;box-sizing: border-box; z-index: 9999;"><div id="scrollamt" style="background: linear-gradient(45deg, rgba(255,179,186,0.8), rgba(255,223,186,0.8), rgba(255,255,186,0.8), rgba(186,255,201,0.8), rgba(186,225,255,0.8), rgba(201,186,255,0.8), rgba(255,186,255,0.8)); outline: 2px rgba(255, 255, 255, 0.57) solid; outline-offset:-2px; background-size: cover; opacity: 0.7; width:100%; height: 0;"></div><div style="position:absolute; transform: rotate(90deg); font-size:1vw; left: 0.55vw; transform-origin: center left; pointer-events: none; white-space: nowrap; cursor:unset;">hi, imagine not being able to raed this... WAIT. DONT SCROLL!</div></div>') }
-const scrollbar = document.getElementById('scrollamt');
-const scrollthing = document.getElementById('scrollthing');
-
-function resizescroll() {
-                const scrollPosition = window.scrollY;
-                const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
-                scrollbar.style.height = `${scrollPosition / scrollableHeight * 100}%`;
-        }
-window.addEventListener('scroll', resizescroll);
-scrollthing.addEventListener('click', function(event) {
-    const rect = scrollthing.getBoundingClientRect();
-    const y = event.clientY - rect.top;
-    const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const scrollPercent = y / rect.height;
-    window.scrollTo(0, scrollPercent * scrollableHeight);
-    console.log("yes")
-});
-let isDragging = false;
-let startY;
-let startScroll;
-scrollamt.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startY = e.clientY;
-    startScroll = window.scrollY;
-    e.preventDefault();
-});
-
-window.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-
-    const deltaY = e.clientY - startY;
-    const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const trackHeight = window.innerHeight;
-    const scrollDelta = (deltaY / trackHeight) * scrollableHeight;
-
-    window.scrollTo(0, startScroll + scrollDelta);
-});
-
-window.addEventListener('mouseup', () => {
-    isDragging = false;
-});
-window.addEventListener('resize', resizescroll);
 
 function russian() {
     document.getElementById("langel-wip").innerText = "[не работает...]";
     document.getElementById("langel-bccreation").innerText = "БК уже";
     document.getElementById("langel-boatcreation").innerText = "БОАТ уже";
     document.getElementById("langel-websitecreation").innerText = "Сайт уже";
-    document.getElementById("langel-katdob").innerText = "пизда, кат родился";
     for (let elemnt of document.getElementsByName("langel-since").values()) {
         elemnt.innerText = "от";
     }
@@ -205,7 +110,6 @@ function russian() {
     document.getElementById("englishbut").innerText = "english";
     document.getElementById("langel-introduction").innerHTML = "<h2>введение</h2>привет, я <b>katrewkate</b> <sub>[обычно сокращается до kat; kate; ktrwkat, в проектах обычно пишется белым]</sub>, с альтернативными именами: <b>vafelkis</b> <sub>[сокращается до vaf, в творческих проектах и других работах обычно пишется оранжевым]</sub>. другие псевдонимы устарели или больше не используются, и упоминаться здесь не будут. добро пожаловать на этот сайт, здесь вы найдете много всего, от информации о предыдущих проектах до статистики, возможно, даже фотографии, кто знает? вот так.</p>";
     displayTimeSinceEvent(new Date("2024-09-19"), 'boatcreationdateago', null, "ru");
-    displayTimeSinceEvent(new Date("2007-09-26"), 'katdob', null, "ru");
     displayTimeSinceEvent(new Date("2022-05-30"), 'bccreationdateago', null, "ru");
     displayTimeSinceEvent(new Date("2025-01-15"), 'websitecreationago', null, "ru");
 }
@@ -214,8 +118,6 @@ function lithuanian() {
     document.getElementById("langel-bccreation").innerText = "bc sukūrimas";
     document.getElementById("langel-boatcreation").innerText = "boat'o įkūrimas";
     document.getElementById("langel-websitecreation").innerText = "puslapio įkūrimas:3";
-    document.getElementById("langel-katdob").innerText = "katman1993 gimė";
-
     for (let elemnt of document.getElementsByName("langel-since").values()) {
         elemnt.innerText = "nuo";
     }
@@ -228,7 +130,6 @@ function lithuanian() {
 
     displayTimeSinceEvent(new Date("2024-09-19"), 'boatcreationdateago', null, "lt");
     displayTimeSinceEvent(new Date("2022-05-30"), 'bccreationdateago', null, "lt");
-    displayTimeSinceEvent(new Date("2007-09-26"), 'katdob', null, "lt");
     displayTimeSinceEvent(new Date("2025-01-15"), 'websitecreationago', null, "lt");
 }
 function english() {
@@ -236,7 +137,6 @@ function english() {
     document.getElementById("langel-bccreation").innerText = "creation of bc";
     document.getElementById("langel-boatcreation").innerText = "creation of boat";
     document.getElementById("langel-websitecreation").innerText = "creation of this website";
-    document.getElementById("langel-katdob").innerText = "birth of katman1993?";
     document.getElementById("langel-vilnius").innerText = "vilnius";
     document.getElementById("langel-links").innerText = "links";
     document.getElementById("langel-introduction").innerHTML = "<h2>introduction</h2>hi, i'm <b>katrewkate</b> <sub>[albeit usually shortened to kat; kate; ktrwkat, in projects usually written in white]</sub>, with alternate names being: <b>vafelkis</b> <sub>[shortened to vaf, on creations and other projects usually written in orange]</sub>. other aliases are discontinued or are obsolete and will not be mentioned here. welcome to this website, here you will find a lot of things, anything ranging from information of previous projects to statistics, maybe even photos, who fucking knows? that's right.</p>";
@@ -251,10 +151,10 @@ function english() {
     document.getElementById("englishbut").innerText = "english [selected]";
     displayTimeSinceEvent(new Date("2024-09-19"), 'boatcreationdateago', null, "en");
     displayTimeSinceEvent(new Date("2025-01-15"), 'websitecreationago', null, "en");
-    displayTimeSinceEvent(new Date("2007-09-26"), 'katdob', null, "en");
     displayTimeSinceEvent(new Date("2022-05-30"), 'bccreationdateago', null, "en");
 }
-
+let date1 = document.getElementById("date1");
+let date2 = document.getElementById("date2");
 function startclock() {
     const nowvln = new Date().toLocaleString('en-GB', {
         timeZone: 'Europe/Vilnius',
@@ -268,61 +168,58 @@ function startclock() {
     });
     const [date, time] = nowvln.split(', ');
     const [day, month, year] = date.split('/');
-    if (Math.floor(Math.random() * 100) === 18) {
-        date1.innerText = `hi, world :)`
-        date2.innerText = "bilnius on top";
-    }
-    else {
-
-        date1.innerText = `${year}-${month}-${day}`
-        date2.innerText = time;
-    }
-    console.log(`${year}-${month}-${day}` + " " + time)
-
+    date1.innerText = `${year}-${month}-${day}`
+    date2.innerText = time;
 }
 const audio = document.getElementById("msc");
 const txtaud = document.getElementById("stopmsc");
-
 function playmsc() {
-    setTimeout(function () {
-        // Code you want to run after 5 seconds
-        audio.play();
+    setTimeout(function() {
+  // Code you want to run after 5 seconds
+  audio.play();
     }, 5000);
-    if (txtaud.innerText.trim() !== "") {
-        txtaud.innerText = "stop background music:3"
-    }
+if (txtaud.innerText.trim() !== "")
+        {
+    txtaud.innerText = "stop background music:3"
+        }
     else {
         let wtv = txtaud.getAttribute("src");
-        if (wtv === "/2abc.png") {
-            txtaud.setAttribute("src", "/2abc_ntsc.png");
+        if (wtv === "../2abc.png")
+        {
+            txtaud.setAttribute("src", "../2abc_ntsc.png");
         }
     }
-
+    
 }
 
-function stopmsc(before = null, after = null) {
+function stopmsc() {
 
     if (audio.paused) {
         audio.play();
-        if (txtaud.innerText.trim() !== "") {
-            txtaud.innerText = after || "stop background music:3";
+        if (txtaud.innerText.trim() !== "")
+        {
+        txtaud.innerText = "stop background music:3";
         }
         else {
-            let wtv = txtaud.getAttribute("src");
-            if (wtv === "/2abc.png") {
-                txtaud.setAttribute("src", "/2abc_ntsc.png");
-            }
-        }
-    } else {
-        audio.pause();
-        if (txtaud.innerText.trim() !== "") {
-            txtaud.innerText = before || "continue background music:3";
-        }
-        else {
-            let wtv = txtaud.getAttribute("src");
-            if (wtv === "/2abc_ntsc.png") {
-                txtaud.setAttribute("src", "/2abc.png");
-            }
+        let wtv = txtaud.getAttribute("src");
+        if (wtv === "../2abc.png")
+        {
+            txtaud.setAttribute("src", "../2abc_ntsc.png");
         }
     }
+    } else {
+        audio.pause();
+        if (txtaud.innerText.trim() !== "")
+        {
+        txtaud.innerText = "continue background music:3";
+        }
+        else {
+        let wtv = txtaud.getAttribute("src");
+        if (wtv === "../2abc_ntsc.png")
+        {
+            txtaud.setAttribute("src", "../2abc.png");
+        }
+    }
+    }
 }
+setInterval(startclock, 1000); 
